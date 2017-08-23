439085056b8213805176f1e97ee0a418aed6b999
Nikita <iamlilacpenguin@gmail.com>
2017-08-23 16:59:22 +0300

    Linux/MacOS support

--- update-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o mods.txt https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
-for mod in $(cat mods.txt) ; do
-	name=$(basename $mod)
-	curl $mod -o $HOME/Library/Application\ Support/minecraft/mods/$name
-done
---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OS="`uname`"
+case $OS in
+  'Linux')
+    minecraft_dir=$HOME/.minecraft/
+    ;;
+  'Darwin') 
+    minecraft_dir=$HOME'/Library/Application Support/minecraft'
+    ;;
+esac
+
+mods_dir="$minecraft_dir/mods"
+
+echo Updating mods.txt
+curl -o mods.txt https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
+for mod in $(cat mods.txt) ; do
+	name=$(basename $mod)
+	echo Downloading "$mod" to "$mods_dir"
+	curl "$mod" -o "$mods_dir/$name"
+done
