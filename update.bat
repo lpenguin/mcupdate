61842b0bab08c7dca00fd75687c6e6bc7e1c25a0
Nikita Prianichnikov <lpenguin@yandex.ru>
2017-08-23 02:47:38 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,11 +1,11 @@
-aria2\aria2c.exe ^
+\aria2c.exe ^
 --allow-overwrite=true ^
 --auto-file-renaming=false ^
 --remove-control-file=true ^
 https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
 
 for /F "tokens=*" %%A in (mods.txt) do (
-	aria2\aria2c.exe ^
+	aria2c.exe ^
 	--allow-overwrite=true ^
 	--auto-file-renaming=false ^
 	--remove-control-file=true ^
