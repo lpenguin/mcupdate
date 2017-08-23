2055158873e990af6142adb06641865f58a627ff
Nikita Prianichnikov <lpenguin@yandex.ru>
2017-08-23 02:46:43 -0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,7 +1,14 @@
+aria2\aria2c.exe ^
+--allow-overwrite=true ^
+--auto-file-renaming=false ^
+--remove-control-file=true ^
+https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
+
 for /F "tokens=*" %%A in (mods.txt) do (
 	aria2\aria2c.exe ^
+	--allow-overwrite=true ^
 	--auto-file-renaming=false ^
 	--remove-control-file=true ^
-	-d C:\Users\IEUser\AppData\Roaming\.minecraft\mods ^
+	-d %HOMEPATH%\AppData\Roaming\.minecraft\mods ^
 	%%A
 )
