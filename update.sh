404a8fe8a109172de0d9d4896f50a3ac00897282
Nikita Prianichnikov <lpenguin@yandex.ru>
2017-08-25 12:02:45 +0300

    curl: following redirects

--- update.sh
+++ update.sh
@@ -13,9 +13,9 @@ esac
 mods_dir="$minecraft_dir/mods"
 
 echo Updating mods.txt
-curl -o mods.txt https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
+curl -L -o mods.txt https://raw.githubusercontent.com/lpenguin/mcupdate/master/mods.txt
 for mod in $(cat mods.txt) ; do
 	name=$(basename $mod)
 	echo Downloading "$mod" to "$mods_dir"
-	curl "$mod" -o "$mods_dir/$name"
+	curl -L "$mod" -o "$mods_dir/$name"
 done
